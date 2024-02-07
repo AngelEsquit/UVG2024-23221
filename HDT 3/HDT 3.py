896519f2631a3666a521a54079b8b511c95ac200
AngelEsquit <esq23221@uvg.edu.gt>
2024-02-06 18:30:25 -0600

    Creación del generador de números

---
+++ HDT 3/HDT 3.py
@@ -0,0 +1,17 @@
+import random
+import csv
+
+# Generar una lista de números enteros al azar
+numeros = [random.randint(0, 10000) for _ in range(3000)]
+
+# Especificar la ruta del archivo CSV
+ruta_archivo = "numeros.csv"
+
+# Guardar los números en el archivo CSV
+with open(ruta_archivo, "w", newline="") as archivo:
+    escritor_csv = csv.writer(archivo)
+    escritor_csv.writerow(["Numeros"])  # Escribir encabezado
+    escritor_csv.writerows([[numero] for numero in numeros])  # Escribir números
+
+print("Los números se han guardado exitosamente en el archivo CSV.")
+
